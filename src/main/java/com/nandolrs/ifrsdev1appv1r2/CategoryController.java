ea55d9fdadf6770597363c591a2984f9ab57dc0b
Fernando Rodrigues <34346597+nandolrs@users.noreply.github.com>
2021-05-15 20:58:42 -0300

    inicio spring pelado com web apenas

--- src/main/java/com/nandolrs/ifrsdev1appv1r2/CategoryController.java
+++ src/main/java/com/nandolrs/ifrsdev1appv1r2/CategoryController.java
@@ -20,24 +20,4 @@ public class CategoryController {
         // This returns a JSON or XML with the users
         return "funcionou" ;
     }
-
-    @PostMapping(
-            value = "teste"
-            ,consumes = {MediaType.APPLICATION_JSON_VALUE}
-            ,produces = {MediaType.APPLICATION_JSON_VALUE}
-    ) // Map ONLY POST Requests
-    public @ResponseBody com.nandolrs.ifrsdev1appv1r2.Category Teste (@RequestBody  com.nandolrs.ifrsdev1appv1r2.Category entidade) {
-        // @ResponseBody means the returned String is the response, not a view name
-        // @RequestParam means it is a parameter from the GET or POST request
-
-
-        entidade.id = 33;
-
-        return entidade;
-    }
-
-
-
-
-
 }
