d5942ea8b817af05631724ecae9c90979ed0b6cb
Fernando Rodrigues <34346597+nandolrs@users.noreply.github.com>
2021-05-15 19:01:54 -0300

    inicio spring pelado com web apenas

---
+++ src/main/java/com/nandolrs/ifrsdev1appv1r2/IfrsDev1Appv1r2Application.java
@@ -0,0 +1,13 @@
+package com.nandolrs.ifrsdev1appv1r2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfrsDev1Appv1r2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfrsDev1Appv1r2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nandolrs/ifrsdev1appv1r2/IfrsDev1Appv1r2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nandolrs.ifrsdev1appv1r2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfrsDev1Appv1r2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
